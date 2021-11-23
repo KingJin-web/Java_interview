b35694f327b06aa673213b24972359335db33d81
KingJin-web <1261337209@qq.com>
2021-11-23 18:36:39 +0800

    ✔ 初次提交

---
+++ src/test/java/com/king/java_interview/JavaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.java_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
