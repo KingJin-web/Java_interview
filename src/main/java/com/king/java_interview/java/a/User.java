b8d8e53f1a4096326e44af82a94194dd72c84d3a
KingJin-web <1261337209@qq.com>
2021-11-24 01:34:56 +0800

    ✔ 1、Java获得对象的几种方式

---
+++ src/main/java/com/king/java_interview/java/a/User.java
@@ -0,0 +1,38 @@
+package com.king.java_interview.java.a;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @program: java_interview
+ * @description:
+ * @author: King
+ * @create: 2021-11-23 20:36
+ */
+@Data
+public class User implements Serializable, Cloneable {
+    private String name;
+    private String sex;
+    private Integer age;
+
+    public void init(String name, String sex, Integer age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public void println() {
+        System.out.println(this);
+    }
+
+    @Override
+    protected User clone() {
+        try {
+            return (User) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/king/java_interview/java/a/UserTest.java
@@ -0,0 +1,90 @@
+package com.king.java_interview.java.a;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Queue;
+import java.util.Stack;
+
+
+@SpringBootTest
+public class UserTest {
+    @Test
+    public void test1() {
+        User user = new User();
+
+    }
+
+
+    @Test
+    @DisplayName("java反射")
+    public void test2() {
+        try {
+            //  1.使用Class类的newInstance方法来创建对象
+            Class class1 = Class.forName("com.king.java_interview.java.a.User");
+            User user1 = (User) class1.newInstance();
+            user1.init("test1", "男", 12);
+            System.out.println(user1);
+
+            //   2.使用Constructor类的newInstance方法来创建兑现
+            Class class2 = Class.forName("com.king.java_interview.java.a.User");
+            Constructor constructor = class2.getConstructor();
+            User user2 = (User) constructor.newInstance();
+            user2.init("test2", "男", 12);
+            System.out.println(user2);
+
+        } catch (ClassNotFoundException | InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    @DisplayName("通过clone的方式")
+    public void test3() {
+        User user1 = new User();
+        user1.init("test1", "男", 12);
+
+        User user2 = user1.clone();
+        System.out.println(user2);
+
+        //这两个对象不是同一个对象
+        System.out.println(user1 == user2);
+
+    }
+
+    @Test
+    @DisplayName("反序列化")
+    public void test4() {
+
+        User user = new User();
+        user.init("king", "男", 12);
+        //准备一个文件用于存储该对象的信息
+        File f = new File("hello.obj");
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+        try (FileOutputStream fos = new FileOutputStream(f);
+             ObjectOutputStream oos = new ObjectOutputStream(fos);
+             FileInputStream fis = new FileInputStream(f);
+             ObjectInputStream ois = new ObjectInputStream(fis)
+        ) {
+            //序列化对象，写入到磁盘中
+            oos.writeObject(user);
+            //反序列化对象
+            User newUser = (User) ois.readObject();
+
+            //测试方法
+            newUser.println();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
