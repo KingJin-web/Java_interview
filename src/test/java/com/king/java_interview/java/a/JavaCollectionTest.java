283be1aaa5e6459fdeee3a2793c50846999cf9f4
KingJin-web <1261337209@qq.com>
2022-01-22 00:51:13 +0800

    ✔ Java的集合框架 list

--- src/test/java/com/king/java_interview/java/a/JavaCollectionTest.java
+++ src/test/java/com/king/java_interview/java/a/JavaCollectionTest.java
@@ -1,5 +1,6 @@
 package com.king.java_interview.java.a;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
@@ -22,5 +23,38 @@ public class JavaCollectionTest {
         Set<Integer> set2 = new TreeSet<>();
         Set<Integer> set3 = new LinkedHashSet<>();
         Set<SeasonEnum> set4 = EnumSet.noneOf(SeasonEnum.class);
+    }
+
+    @Test
+    public void Test1() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        //第一种遍历方法使用 For-Each 遍历 List
+        for (Integer str : list) {            //也可以改写 for(int i=0;i<list.size();i++) 这种形式
+            System.out.println(str);
+        }
+        list.forEach(System.out::println);
+
+        //第二种遍历，把链表变为数组相关的内容进行遍历
+        Integer[] strArray = new Integer[list.size()];
+        list.toArray(strArray);
+
+        for (Integer s : strArray) { //这里也可以改写为      for(int i=0;i<strArray.length;i++) 这种形式
+            System.out.println(s);
+        }
+
+        //第三种遍历 使用迭代器进行相关遍历
+        Iterator<Integer> ite = list.iterator();
+        while (ite.hasNext()) {//判断下一个元素之后有值
+            System.out.println(ite.next());
+        }
+
+    }
+
+    @Test
+    public void test2(){
+
     }
 }
